fbfcf4230de7629e0533a6a73e8a599dab143aee
artemis <artemisxen@gmail.com>
2017-09-05 11:08:05 +0100

    added test for negative amount

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,22 @@
 class Account
   attr_reader :balance, :transactions
 
+  MIN_BALANCE = 0
+
   def initialize(statement = Statement.new)
-    @balance = 0
+    @balance = MIN_BALANCE
     @transactions = []
     @statement = statement
   end
 
   def deposit(amount)
+    raise "Sorry, the amount can't be negative!" if amount < MIN_BALANCE
     @balance += amount
     @transactions << { date: date, credit: amount, balance: balance }
   end
 
   def withdraw(amount)
+    raise "Sorry, the amount can't be negative!" if amount < MIN_BALANCE
     raise 'Sorry, not enough balance!' if amount > @balance
     @balance -= amount
     @transactions << { date: date, debit: amount, balance: balance }
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,10 +5,11 @@ describe Account do
   let(:statement) { double:statement, print: '10/7/2017 || 300 || || 300' }
   let(:deposit_amount) { 500 }
   let(:withdrawal_amount) { 300 }
+  let(:wrong_amount) { -400.30 }
 
   context '#initialize' do
     it 'has 0 initial balance' do
-      expect(account.balance).to eq 0
+      expect(account.balance).to eq described_class::MIN_BALANCE
     end
 
     it 'starts with empty array of transactions' do
@@ -25,6 +26,10 @@ describe Account do
 
     it 'stores the deposit data in transactions' do
       expect(account.transactions.length).to eq 1
+    end
+
+    it "the amount can't be negative" do
+      expect { account.deposit(wrong_amount) }.to raise_error("Sorry, the amount can't be negative!")
     end
   end
 
@@ -45,6 +50,11 @@ describe Account do
     it "can't withdraw amount less than the balance" do
       expect { account.withdraw(withdrawal_amount) }.to raise_error('Sorry, not enough balance!')
     end
+
+    it "the amount can't be negative" do
+      expect { account.withdraw(wrong_amount) }.to raise_error("Sorry, the amount can't be negative!")
+    end
+
   end
 
   context '#print_statement' do
